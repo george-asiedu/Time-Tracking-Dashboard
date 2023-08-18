c84110c9f96cec319b46217ff952dcbc462545d2
George Kwabena Asiedu <asiedug41@gmail.com>
2023-08-18 02:18:43 +0000

    active button js update

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const buttons = document.querySelectorAll('.btn')
+
 let jsonData = []
 
 const receiveData = async () => {
     const response = await fetch('./data.json')
     const fetchedData = await response.json()
     jsonData = fetchedData
-    console.log(jsonData);
 }
+
+const clickButtons = btnArray => {
+    btnArray.forEach(button => {
+        button.addEventListener('click', () => {
+            activeButton(button)
+            const activeButtonOption = button.dataset.option
+            dashboardContent(activeButtonOption)
+        })
+    })
+}
+
+const activeButton = active => {
+    buttons.forEach(btn => btn.classList.remove('active'))
+    active.classList.add('active')
+}
+
+
+
+clickButtons(buttons)
 receiveData()
